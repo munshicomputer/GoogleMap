414fc82053391d7e9ec9309cc525529eb941fd92
Shamim Munshi <munshi6668@gmail.com>
2023-08-07 13:15:54 +0600

    User Current Location Get Implemantation.

--- app/src/main/java/com/munshisoft/googlemap/MainActivity.java
+++ app/src/main/java/com/munshisoft/googlemap/MainActivity.java
@@ -1,18 +1,87 @@
 package com.munshisoft.googlemap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private final static int REQUEST_CODE = 100;
 
+    FusedLocationProviderClient fusedLocationProviderClient;
+    TextView tvAddress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Fragment fragment = new MapFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
+
+        tvAddress = findViewById(R.id.tv_address);
+
+        /** MARK: - Load Google MAP on Fragment*/
+//        Fragment fragment = new MapFragment();
+//        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
+
+        getCurrentLocation();
+
+    }
+
+    private void getCurrentLocation() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null){
+                                Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                                try {
+                                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(),1);
+                                    tvAddress.setText("Address: \nLatitude: "+ addresses.get(0).getLatitude()
+                                    +"\nLongitude: "+addresses.get(0).getLongitude()
+                                    +"Address: "+ addresses.get(0).getAddressLine(0)
+                                    +"City: "+ addresses.get(0).getLocality()
+                                    +"Country: "+ addresses.get(0).getCountryName());
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                        }
+                    });
+        }else{
+            askPermission();
+        }
+    }
+
+    private void askPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }else{
+                Toast.makeText(this, "Location Permission Required.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
